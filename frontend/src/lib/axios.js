a7a0d2c412caa8f7a37af4820ae6c6b9252136c9
vineeth483 <vineeth0483@gmail.com>
2025-06-13 02:35:15 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // in production, there's no localhost so we have to make this dynamic
-const BASE_URL = import.meta.env.MODE === "development" ? "https://note-stack-oi41-9nynszrgp-vineethbammidis-projects.vercel.app/" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://note-stack-oi41-9nynszrgp-vineethbammidis-projects.vercel.app" : "/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
