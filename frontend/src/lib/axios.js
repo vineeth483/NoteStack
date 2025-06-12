75ed957c1777986364b63c5c92a8079e4a58a5a3
vineeth483 <vineeth0483@gmail.com>
2025-06-13 02:59:20 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development"
-  ? "http://localhost:5000" // for local testing
-  : "https://note-stack-oi41.vercel.app"; // 👈 your actual backend URL
+  ? "http://localhost:5000"
+  : "https://note-stack-oi41.vercel.app"; // your deployed backend URL
 
 const api = axios.create({
   baseURL: BASE_URL,
