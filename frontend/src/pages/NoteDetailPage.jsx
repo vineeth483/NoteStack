1bd8b2261cc4bfc2e710e8ebfa607546565b87c7
vineeth483 <vineeth0483@gmail.com>
2025-06-13 16:40:08 +0530

    Update NoteDetailPage.jsx

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -18,7 +18,7 @@ const NoteDetailPage = () => {
     const fetchNote = async () => {
       try {
 
-        const res = await api.get(`/api/notes/${id}`);
+        const res = await api.get(`/notes/${id}`);
 
         setNote(res.data);
       } catch (error) {
