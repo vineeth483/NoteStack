10f2667764d417ca1ee4c9f76908041add3d98ef
vineeth483 <vineeth0483@gmail.com>
2025-06-13 16:18:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
-    }
-  }
-=======
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 2b6054c8d26229f4a51a364eb8f4604f0086ec7a
+      '/api': 'http://localhost:5000',
+    },
+  },
 })
